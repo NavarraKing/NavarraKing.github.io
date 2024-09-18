f9f4b57f6ef07e3521863198d5e1d28e9dd111ec
NavarraKing <124450874+NavarraKing@users.noreply.github.com>
2024-09-19 00:33:09 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("registerForm").addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value; 
+
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+    document.getElementById("result").innerHTML = `
+    <h3>Válasz a szervertől:</h3>
+    <p>Név: ${result.name}</p>
+    <p>E-mail: ${result.email}</p>`;
+    })
+    .catch(error => {
+        document.getElementById('result').innerHTML = `<p>Hiba történt: ${error.message}</p>`;
+    });
+})
